b3f631fdd20f35da46d8212bd922080a003fb305
Mubeen Ahmed <mubeen.ahmed>
2020-10-04 01:15:57 +0500

    CSV Reading

--- src/main/scala/grub/io/CSVReader.scala
+++ src/main/scala/grub/io/CSVReader.scala
@@ -1,8 +1,11 @@
 package grub.io
 
+import java.util.regex.Pattern
+
 import grub.structure.{ArrayBasedIndex, DataFrame, Index}
 
 import scala.io.Source
+import scala.util.matching.Regex
 
 
 case class CSVReadOption(fileName: String,
@@ -11,25 +14,33 @@ case class CSVReadOption(fileName: String,
                             names: Option[List[String]] = None,
                             indexCol: Option[String] = None)
 class CSVRead {
+
   this: GrubIO =>
-  def read(option: CSVReadOption): DataFrame[String] = {
+
+  def read(option: CSVReadOption): DataFrame[Any] = {
     val src = source(option.fileName)
     val columnNames = includeHeader(src, option)
     val columns: List[(String, Int)] = columnNames.zipWithIndex
 
     val body: Seq[Array[String]] = src.map(_.split(option.delimiter)).toSeq
 
-    val data = getSeq(columns, body)
     val colIndex  = getIndex(columnNames, option)
     val index = addIndex(colIndex, body)
 
-    if(index.isDefined) DataFrame[String](data, columnNames, index.get)
-    else DataFrame[String](data, columnNames)
+    val data = getSeq(columns, body)
+
+    if(index.isDefined) DataFrame[Any](data, columnNames, index.get)
+    else DataFrame[Any](data, columnNames)
   }
 
-  private def getSeq(columns: List[(String, Int)], body: Seq[Array[String]]): Seq[Seq[String]]  = for {
+  private def getSeq(columns: List[(String, Int)], body: Seq[Array[String]]): Seq[Seq[Any]]  = for {
     (_, c) <- columns
-    cols = body.map(x => x(c))
+    cols = body.map {
+      case x: Array[String] =>
+        if(isIntType(x(c))) getIntType(x(c))
+        else if (isDoubleType(x(c))) getDoubleType(x(c))
+        else x(c)
+    }
   } yield cols
 
   private def getIndex(columnNames: List[String], option: CSVReadOption): Int =
@@ -45,7 +56,27 @@ class CSVRead {
   private def includeHeader(src: Iterator[String], option: CSVReadOption): List[String] =
     if(option.includeHeader) src.take(1).map(_.split(option.delimiter)).next().toList
     else option.names.getOrElse(throw new IllegalArgumentException("Add columns names"))
+
+
+  private def getIntType(value: String): Int = value.toInt
+  private def getDoubleType(value: String): Double = value.toDouble
+
+  private def isIntType(value: String): Boolean = try {
+    value.toInt
+    true
+  } catch {
+    case _: Exception => false
+  }
+
+  private def isDoubleType(value: String): Boolean = try {
+    value.toDouble
+    true
+  } catch {
+    case _: Exception => false
+  }
+
 }
+
 
 trait GrubIO {
   def source(fileName: String): Iterator[String]
--- src/test/scala/grub/io/CSVFileReaderTest.scala
+++ src/test/scala/grub/io/CSVFileReaderTest.scala
@@ -4,26 +4,29 @@ import grub.io
 import grub.structure.DataFrame
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should
+import grub.info.Information.Information
 
 class CSVFileReaderTest extends AnyFlatSpec with should.Matchers
 {
 
   val csvReader = new CSVRead with MockFileSystem
+  val csvReader2 = new CSVRead with MockFileSystem2
+
   val fileName = "/c:/sample.csv"
 
   "Reading from default option" should "create dataframe" in {
     val csvReadingOption = CSVReadOption(fileName)
-    val df: DataFrame[String] = csvReader.read(csvReadingOption)
-    df.data should be (Seq(Seq("1"), Seq("4"), Seq("0.1")) )
+    val df: DataFrame[Any] = csvReader.read(csvReadingOption)
+    df.data should be (Seq(Seq(1), Seq(4), Seq(0.1)) )
   }
 
 
   "Reading with specifying column" should "create dataframe" in {
     val columnNames = List("a", "b", "c")
     val csvReadOption = io.CSVReadOption(fileName = fileName, includeHeader = false, names = Some(columnNames))
-    val df: DataFrame[String] = csvReader.read(csvReadOption)
+    val df: DataFrame[Any] = csvReader.read(csvReadOption)
     df.columns.all should be (columnNames)
-    df.data should be (Seq(Seq("cat", "1"), Seq("dog", "4"), Seq("mouse", "0.1")) )
+    df.data should be (Seq(Seq("cat", 1), Seq("dog", 4), Seq("mouse", 0.1)) )
   }
 
   it should "throw IllegalArgumentException " in {
@@ -34,8 +37,8 @@ class CSVFileReaderTest extends AnyFlatSpec with should.Matchers
   it should "use csv header" in {
     val columnNames = List("a", "b", "c")
     val csvReaderOption = io.CSVReadOption(fileName = fileName, names = Some(columnNames))
-    val df: DataFrame[String] = csvReader.read(csvReaderOption)
-    df.data should be (Seq(Seq("1"), Seq("4"), Seq("0.1")) )
+    val df: DataFrame[Any] = csvReader.read(csvReaderOption)
+    df.data should be (Seq(Seq(1), Seq(4), Seq(0.1)) )
   }
 
 
@@ -44,16 +47,26 @@ class CSVFileReaderTest extends AnyFlatSpec with should.Matchers
     val csvReaderOption2 = io.CSVReadOption(fileName = fileName, indexCol = Some("dog"))
     val csvReaderOption3 = io.CSVReadOption(fileName = fileName, indexCol = Some("mouse"))
 
-    val df1: DataFrame[String] = csvReader.read(csvReaderOption1)
-    val df2: DataFrame[String] = csvReader.read(csvReaderOption2)
-    val df3: DataFrame[String] = csvReader.read(csvReaderOption3)
+    val df1: DataFrame[Any] = csvReader.read(csvReaderOption1)
+    val df2: DataFrame[Any] = csvReader.read(csvReaderOption2)
+    val df3: DataFrame[Any] = csvReader.read(csvReaderOption3)
 
     df1.index.index.get("1") should be (Some(0))
     df2.index.index.get("4") should be (Some(0))
     df3.index.index.get("0.1") should be (Some(0))
   }
 
+  "Reading from different data csv" should "return same result" in {
+    val csvReadingOption = CSVReadOption(fileName)
+    val df = csvReader2.read(csvReadingOption)
+    df.describe
+  }
+
   trait MockFileSystem extends GrubIO {
     override def source(fileName: String): Iterator[String] = Seq("cat;dog;mouse;", "1;4;0.1").iterator
+  }
+
+  trait MockFileSystem2 extends GrubIO {
+    override def source(fileName: String): Iterator[String] =  Seq("cat;dog;mouse;", "1;4;0.1", "abc;;1").iterator
   }
 }
